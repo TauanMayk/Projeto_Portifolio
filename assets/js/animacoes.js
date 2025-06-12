104c89a42559f9579bf1a8a3ae84e4606e1ce5d0
Tauan Mayk <tauanmaykreal@gmail.com>
2025-06-12 20:09:15 -0300

    Melhorando o styles do projeto e adicionando mais animações

--- assets/js/animacoes.js
+++ assets/js/animacoes.js
@@ -1,6 +1,17 @@
 const logosSVG = document.querySelectorAll(".technologies__logo");
 const icons = document.querySelectorAll('.tech-icon');
 const btnProjetos = document.getElementsByClassName('btn--primary');
+const revealElements = document.querySelectorAll('.reveal');
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
 
 window.addEventListener('load', () => {
     document.body.classList.add('loaded');
@@ -10,4 +21,10 @@ icons.forEach((icon, index) => {
   setTimeout(() => {
     icon.classList.add('show');
   }, index * 150);
+});
+
+revealElements.forEach((el, i) => {
+  setTimeout(() => {
+    el.classList.add('visible');
+  }, i * 300);
 });
